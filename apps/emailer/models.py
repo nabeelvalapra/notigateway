4f61c46b0e76457be982fc4d25ac6708fe8598ad
Nabeel Valapra <nabeelvalapra@gmail.com>
2020-02-24 15:03:43 +0530

    Implemented Notification Model

--- apps/emailer/models.py
+++ apps/emailer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Action(models.TextChoices):
+    SAVE = 'SV', 'Save'
+    LOGGING = 'LG', 'Logging'
+    DELIVERY = 'DV', 'Delivery'
+    POST_DELIVERY = 'PD', 'Post Delivery Status'
+    REPORT_GEN  = 'RD', 'Report Generation'
+
+
+class Notification(models.Model):
+    action = models.CharField(max_length=2, choices=Action.choices)
+    recipient = models.EmailField()
+    subject = models.TextField()
+    body = models.TextField()
+
+    class Meta:
+        db_table = 'notification'
+
+
+
