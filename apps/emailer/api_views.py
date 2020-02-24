9c9fdbf1d2fb46d182b53d271ac9b0f71fe25d4e
Nabeel Valapra <nabeelvalapra@gmail.com>
2020-02-24 23:18:11 +0530

    cleans the data and saves - Notifications

--- apps/emailer/api_views.py
+++ apps/emailer/api_views.py
@@ -1,6 +1,7 @@
 # DRF imports
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import status
 
 # App imports
 from emailer.serializers import NotificationSerializer
@@ -8,13 +9,17 @@ from emailer.serializers import NotificationSerializer
 
 class SendNotification(viewsets.ViewSet):
     """
-    /api/v1/send-notification/ accepts notification-data
+    /send-notification/ accepts notification-data
     and pushes to queue for the processing.
     """
     serializer_class = NotificationSerializer
     http_method_names = ['post', ]
 
     def create(self, request):
-        return Response({})
+        notif =  self.serializer_class(data=request.data)
+        if notif.is_valid():
+            notif.save()
+            return Response(notif.data, status=status.HTTP_206_PARTIAL_CONTENT)
+        return Response(notif.error)
         
 
